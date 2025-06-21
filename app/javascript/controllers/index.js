8a5888615bf09bfde8c466821f5b10c7e389df6f
aim2bpg <honotaka2016@gmail.com>
2025-06-21 19:28:13 +0900

    rails g stimulus regexp

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import RegexpController from "./regexp_controller"
+application.register("regexp", RegexpController)
