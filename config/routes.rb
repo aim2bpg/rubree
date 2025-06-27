8a5bfe19bf32a8fafd07646807d6fcfed5213af9
aim2bpg <58363353+aim2bpg@users.noreply.github.com>
2025-06-28 01:09:28 +0900

    Change pwa/* path for github pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "regular_expressions#index"
 
   resources :regular_expressions, only: %i[index create]
+  get "*path", to: "regular_expressions#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
