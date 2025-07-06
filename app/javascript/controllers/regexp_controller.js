18eaa76364d7639e8d680ed1328b83c29baa6c97
aim2bpg <58363353+aim2bpg@users.noreply.github.com>
2025-07-06 13:29:12 +0900

    yarn exec biome check --write (..manual care)

--- app/javascript/controllers/regexp_controller.js
+++ app/javascript/controllers/regexp_controller.js
@@ -1,12 +1,12 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="regexp"
 export default class extends Controller {
   submit() {
-    clearTimeout(this.timeout)
+    clearTimeout(this.timeout);
 
     this.timeout = setTimeout(() => {
-      this.element.requestSubmit()
-    }, 200)
+      this.element.requestSubmit();
+    }, 200);
   }
 }
