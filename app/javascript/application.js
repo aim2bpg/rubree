e37e91b613e39c27aa39567192718f108760aa18
aim2bpg <58363353+aim2bpg@users.noreply.github.com>
2025-07-13 15:43:07 +0900

    Add wrap toggle and show invisibles toggle for regex match display

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,20 @@
 import "@hotwired/turbo-rails";
 import "./controllers";
 import "./copy_code";
+import Alpine from "alpinejs";
+
+window.Alpine = Alpine;
+Alpine.start();
+
+document.addEventListener("alpine:init", () => {
+  Alpine.data("matchComponent", () => ({
+    wrap: localStorage.getItem("wrap") === "false" ? false : true,
+    showInvisibles: false,
+
+    init() {
+      this.$watch("wrap", (value) => {
+        localStorage.setItem("wrap", value);
+      });
+    },
+  }));
+});
