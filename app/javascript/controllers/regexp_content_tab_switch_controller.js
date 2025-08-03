8e9b09a86a7475be2f55887eded0cd10c2264309
aim2bpg <58363353+aim2bpg@users.noreply.github.com>
2025-08-03 13:56:14 +0900

    Prepare UI section for future substitution feature

--- app/javascript/controllers/regexp_content_tab_switch_controller.js
+++ app/javascript/controllers/regexp_content_tab_switch_controller.js
@@ -5,8 +5,10 @@ export default class extends Controller {
   static targets = [
     "tabReference",
     "tabExamples",
+    "tabSubstitution",
     "referencePanel",
     "examplesPanel",
+    "substitutionPanel",
   ];
 
   connect() {
@@ -16,18 +18,36 @@ export default class extends Controller {
   showReference() {
     this.referencePanelTarget.classList.remove("hidden");
     this.examplesPanelTarget.classList.add("hidden");
+    this.substitutionPanelTarget.classList.add("hidden");
     this.tabReferenceTarget.classList.add("bg-gray-700", "text-white");
     this.tabReferenceTarget.classList.remove("bg-gray-800", "text-gray-300");
     this.tabExamplesTarget.classList.remove("bg-gray-700", "text-white");
     this.tabExamplesTarget.classList.add("bg-gray-800", "text-gray-300");
+    this.tabSubstitutionTarget.classList.remove("bg-gray-700", "text-white");
+    this.tabSubstitutionTarget.classList.add("bg-gray-800", "text-gray-300");
   }
 
   showExamples() {
     this.referencePanelTarget.classList.add("hidden");
     this.examplesPanelTarget.classList.remove("hidden");
+    this.substitutionPanelTarget.classList.add("hidden");
     this.tabExamplesTarget.classList.add("bg-gray-700", "text-white");
     this.tabExamplesTarget.classList.remove("bg-gray-800", "text-gray-300");
     this.tabReferenceTarget.classList.remove("bg-gray-700", "text-white");
     this.tabReferenceTarget.classList.add("bg-gray-800", "text-gray-300");
+    this.tabSubstitutionTarget.classList.remove("bg-gray-700", "text-white");
+    this.tabSubstitutionTarget.classList.add("bg-gray-800", "text-gray-300");
+  }
+
+  showSubstitution() {
+    this.referencePanelTarget.classList.add("hidden");
+    this.examplesPanelTarget.classList.add("hidden");
+    this.substitutionPanelTarget.classList.remove("hidden");
+    this.tabExamplesTarget.classList.remove("bg-gray-700", "text-white");
+    this.tabExamplesTarget.classList.add("bg-gray-800", "text-gray-300");
+    this.tabReferenceTarget.classList.remove("bg-gray-700", "text-white");
+    this.tabReferenceTarget.classList.add("bg-gray-800", "text-gray-300");
+    this.tabSubstitutionTarget.classList.add("bg-gray-700", "text-white");
+    this.tabSubstitutionTarget.classList.remove("bg-gray-800", "text-gray-300");
   }
 }
